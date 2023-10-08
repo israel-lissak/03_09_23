f510025cecfe75c1722fa7f05f7603a504f3bf5d
israel-lissak <islissak@gmail.com>
2023-10-08 14:56:20 +0300

    10_08 first commit

--- react_project_sunday/src/App.tsx
+++ react_project_sunday/src/App.tsx
@@ -9,21 +9,21 @@ import UsersFromServer from './UsersFromServer'
 function App() {
 
   const users = [
-    { id: 1, name: "שם 1", email: "user1@example.com" },
-    { id: 2, name: "שם 2", email: "user2@example.com" },
-    { id: 3, name: "שם 3", email: "user3@example.com" },
-    { id: 4, name: "שם 4", email: "user4@example.com" },
-    { id: 5, name: "שם 5", email: "user5@example.com" },
+    { id: "1", name: "שם 1", email: "user1@example.com" },
+    { id: "2", name: "שם 2", email: "user2@example.com" },
+    { id: "3", name: "שם 3", email: "user3@example.com" },
+    { id: "4", name: "שם 4", email: "user4@example.com" },
+    { id: "5", name: "שם 5", email: "user5@example.com" },
 ];
 
   return (
     <>
-{/*       
+      
       <Counter/>
 
       <StyleChanger/>
 
-      <Users users={users}/> */}
+      <Users users={users}/>
 
       <UsersFromServer/>
 
--- react_project_sunday/src/Users.tsx
+++ react_project_sunday/src/Users.tsx
@@ -1,7 +1,8 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
+import {v4}  from "uuid"
 
 interface User {
-    id: number
+    id: string
     name: string
     email: string
 }
@@ -11,23 +12,45 @@ interface Props {
 
 function Users(props:Props) {
 
+    const [users, setUsers] = useState<User[]>(props.users);
 
+    const nameRef = useRef<HTMLInputElement | null>(null);
+    const emailRef = useRef<HTMLInputElement | null>(null);
     
     function addNewUser():void {
+
+      if (nameRef.current && emailRef.current){
+
+        const newUser = {
+          id: v4(),
+          name: nameRef.current?.value,
+          email: emailRef.current?.value
+        }
+  
+        setUsers((prevUsers) => [...prevUsers, newUser]);
+
+        nameRef.current.value = "";
+        emailRef.current.value = "";
+
+        console.log(props.users);
         
+      }
+
     }
 
   return (
   <>
-    {props.users.map((user,index) => {
-        return <p>{`user ${index + 1}:
-        name: ${user.name}
-        id: ${user.id}
-        email: ${user.email}`}</p>
+      {users.map((user,index) => {
+        return <p>
+                  {`user ${index + 1}:
+                  name: ${user.name}
+                  id: ${user.id}
+                  email: ${user.email}`}
+               </p>
     })}
     
-    <input type="text" placeholder="name"/>
-    <input type="text" placeholder="email"/>
+    <input type="text" placeholder="name" ref={nameRef} />
+    <input type="text" placeholder="email" ref={emailRef}/>
     <button onClick={addNewUser}>add user</button>
   </>
     
